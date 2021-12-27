741c237fc0b7a9e4b52581ad0d6e9b4f6ae264e8
Prakash Muthusamy <prakash.muthusamy@cesltd.com>
2021-12-27 19:35:29 +0530

    modified some codes

--- firstServer.js
+++ firstServer.js
@@ -6,7 +6,10 @@
 const HTTP = require("http")
 
 HTTP.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' }) // some thunder client verification purpose
     res.write("Created my First server!!!")
+    res.write("<h1>hi</h1>")
+
     res.end()
 
 }).listen(3100)
